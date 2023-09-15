7a6d84c5611652cb01533bc51b1534f83851b66f
Adam Fain <adam@rapidtrade.biz>
2023-09-15 11:45:46 +0200

    Update Printer.java

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -21,8 +21,10 @@
 
 package de.appplant.cordova.plugin.printer;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.webkit.WebView;
 
 import org.apache.cordova.CallbackContext;
